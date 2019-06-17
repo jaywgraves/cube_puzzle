2ba5cf05e53fe71cdabb22042037e7d4a9c241c2
jay graves <jaywgraves@gmail.com>
2019-06-17 06:05:43 -0500

    tweaks but also added 'rotate_cube_face_start' which is not working

--- solve.py
+++ solve.py
@@ -1,10 +1,10 @@
 import copy
 import sys
 import random
-from collections import defaultdict
 
 solve_all = True
 shuffle_cube_order = False
+rotate_cube_face_start = False
 
 # normal die
 #  1
@@ -19,17 +19,7 @@ shuffle_cube_order = False
 # 2354  1,2,3,5
 # 2453 rev
 
-cube = []
-#             1   2   3   5   6   4
-cube.append(('G','B','W','W','R','R'))
-cube.append(('B','R','W','R','R','G'))
-cube.append(('B','G','W','R','B','G'))
-cube.append(('G','R','B','B','W','W'))
-if shuffle_cube_order:
-    random.shuffle(cube)
-
 rotations = []
-
 face_seed = []
 face_seed.append([0,1,4,3])
 face_seed.append([0,2,4,5])
@@ -39,8 +29,26 @@ for faces in face_seed:
         rotations.append(copy.copy(faces))
         rotations.append(list(reversed(faces)))
         faces.append(faces.pop(0))
-
 rot_len = len(rotations)
+
+cube = []
+#             1   2   3   5   6   4
+cube.append(('G','B','W','W','R','R'))
+cube.append(('B','R','W','R','R','G'))
+cube.append(('B','G','W','R','B','G'))
+cube.append(('G','R','B','B','W','W'))
+if shuffle_cube_order:
+    random.shuffle(cube)
+# this does not work
+if rotate_cube_face_start:
+    new_cube = []
+    for c in cube:
+        c = list(c)
+        for _ in range(random.randint(0,5)):
+            c.append(c.pop(0))
+        new_cube.append(tuple(c))
+    cube = new_cube
+
 
 def get_cube(cube_idx, rot_idx):
     return tuple(cube[cube_idx][ri] for ri in rotations[rot_idx])
@@ -55,6 +63,7 @@ def check(a, b, c, d):
     return True
 
 cnt = 0
+solve_cnt = 0
 for ai in range(rot_len):
     a = get_cube(0, ai)
     for bi in range(rot_len):
@@ -66,6 +75,7 @@ for ai in range(rot_len):
                 cnt += 1
                 solved = check(a, b, c, d)
                 if solved:
+                    solve_cnt += 1
                     print('solved after',cnt,'test')
                     print(a)
                     print(b)
@@ -74,5 +84,7 @@ for ai in range(rot_len):
                     if not solve_all:
                         sys.exit()
 
+if solve_all:
+    print(solve_cnt, "solutions")
 print('total count of tests', cnt)
 
